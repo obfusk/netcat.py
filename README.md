11f00e16ecfb7ab0c079d1ba304068bd21153352
Felix C. Stegerman <flx@obfusk.net>
2015-09-13 01:23:57 +0200

    v0.1.0
      * add --command
      * better binary r/w
      * use SO_REUSEADDR
      * replace lock with term event
      * only accept one UDP client (still can't receive ICMP though)
      * cleanup & review
      * LGPL -> GPL

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
     File        : README.md
     Maintainer  : Felix C. Stegerman <flx@obfusk.net>
-    Date        : 2015-09-10
+    Date        : 2015-09-12
 
     Copyright   : Copyright (C) 2015  Felix C. Stegerman
-    Version     : v0.0.1
+    Version     : v0.1.0
 
 []: }}}1
 
@@ -21,28 +21,31 @@ See `netcat.py` for the code (with examples).
 
 ```
 $ ./netcat.py -l -p 8888      # server
-hi there                      # user input
-^C
+hi client!                    # typed on stdin
+hi server!                    # from client
+^C                            # bye
 ```
 
 ```
 $ ./netcat.py localhost 8888  # client
-hi there                      # from server
+hi client!                    # from server
+hi server!                    # typed on stdin
 ```
 
 ## TODO
 
-* `-c COMMAND` etc?
-* UDP multiple addrs?!
-* no idle waiting?!
+* find a way to receive ICMP port unreachable etc. so we know when a
+  UDP socket is not connected?
+* prevent idle waiting (only seems to occur with "fake" I/O where
+  select doesn't actually block)?
 
 ## License
 
-LGPLv3+ [1].
+GPLv3+ [1].
 
 ## References
 
-[1] GNU Lesser General Public License, version 3
---- https://www.gnu.org/licenses/lgpl-3.0.html
+[1] GNU General Public License, version 3
+--- https://www.gnu.org/licenses/gpl-3.0.html
 
 []: ! ( vim: set tw=70 sw=2 sts=2 et fdm=marker : )
--- netcat.py
+++ netcat.py
@@ -4,11 +4,11 @@
 #
 # File        : netcat.py
 # Maintainer  : Felix C. Stegerman <flx@obfusk.net>
-# Date        : 2015-09-10
+# Date        : 2015-09-12
 #
 # Copyright   : Copyright (C) 2015  Felix C. Stegerman
-# Version     : v0.0.1
-# License     : LGPLv3+
+# Version     : v0.1.0
+# License     : GPLv3+
 #
 # --                                                            ; }}}1
 
@@ -17,21 +17,27 @@ r"""
 Python (2+3) netcat
 
 Examples
---------
+========
 
->>> port1, port2 = 8888, 9999
+>>> import netcat as N, os, sys, tempfile, time
+>>> port1 = int(os.environ.get("NC_TEST_PORT", 8000)); port2 = port1+1
 
->>> import netcat as N, sys, tempfile, time
+
+HTTP GET request to obfusk.ch
+-----------------------------
 
 >>> s = sys.stdin; f = tempfile.TemporaryFile()
 >>> _ = f.write(b"GET / HTTP/1.0\r\n\r\n"); _ = f.seek(0)
 >>> sys.stdin = f
 
->>> N.client("obfusk.ch", 80)                     # doctest: +ELLIPSIS
+>>> N.client("obfusk.ch", 80)   # <--             # doctest: +ELLIPSIS
 HTTP/1.1 301 Moved Permanently...
 
 >>> sys.stdin = s
 
+
+Talk to netcat.py server
+------------------------
 
 >>> import subprocess, threading
 
@@ -44,11 +50,14 @@ HTTP/1.1 301 Moved Permanently...
 >>> def killer(p): time.sleep(1); p.terminate()
 >>> threading.Thread(target = killer, args = [p]).start()
 
->>> N.client("localhost", port1)
+>>> N.client("localhost", port1)  # <--
 Hi!
 
 >>> sys.stdin = s
 
+
+Talk to netcat.py server via UDP
+--------------------------------
 
 >>> c   = [sys.executable, "netcat.py", "-u", "-l", "-p", str(port2)]
 >>> f1  = tempfile.TemporaryFile(); f2 = tempfile.TemporaryFile()
@@ -56,14 +65,35 @@ Hi!
 >>> _   = f2.write(b"DATA\n"); _ = f2.seek(0)
 >>> p   = subprocess.Popen(c, stdin = f1, stdout = subprocess.PIPE)
 >>> s   = sys.stdin; sys.stdin = f2
+>>> t   = threading.Event()
 >>> time.sleep(1)
->>> def killer(p, f2):
-...   time.sleep(1); p.terminate()
-...   with N.handle_io.lock: f2.close()
->>> threading.Thread(target = killer, args = [p, f2]).start()
+>>> def killer(p, t): time.sleep(1); p.terminate(); t.set()
+>>> threading.Thread(target = killer, args = [p, t]).start()
 
->>> N.client("localhost", port2, udp = True)
+>>> N.client("localhost", port2, udp = True, term = t)  # <--
 Hi!
+
+>>> sys.stdin = s
+
+
+Talk to netcat.py server reversing lines
+----------------------------------------
+
+>>> pc  = "perl -ne '$|=1;chomp;print scalar reverse,\"\n\"'"
+>>> c   = [sys.executable, "netcat.py", "-l", "-p", str(port1), "-c", pc]
+>>> f   = tempfile.TemporaryFile()
+>>> _   = f.write(b"foo\nbar\nbaz\n"); _ = f.seek(0)
+>>> p   = subprocess.Popen(c)
+>>> s   = sys.stdin; sys.stdin = f
+>>> t   = threading.Event()
+>>> time.sleep(1)
+>>> def killer(p, t): time.sleep(1); p.terminate(); t.set()
+>>> threading.Thread(target = killer, args = [p, t]).start()
+
+>>> N.client("localhost", port1, term = t)  # <--
+oof
+rab
+zab
 
 >>> sys.stdin = s
 """
@@ -71,7 +101,7 @@ Hi!
 
 from __future__ import print_function
 
-import argparse, fcntl, os, select, sys, threading
+import argparse, fcntl, os, select, subprocess, sys
 import socket as S
 
 if sys.version_info.major == 2:                                 # {{{1
@@ -98,20 +128,24 @@ else:
     return x.encode("utf8")
   def binread(s, n):
     """read bytes from stream"""
-    if hasattr(s, "buffer"):
+    if hasattr(s, "mode") and "b" in s.mode:
+      return s.read(n)
+    elif hasattr(s, "buffer"):
       return s.buffer.read(n)
     else:
       return s2b(s.read(n))                                     # TODO
   def binwrite(s, data):
     """write bytes to stream"""
-    if hasattr(s, "buffer"):
+    if hasattr(s, "mode") and "b" in s.mode:
+      return s.write(data)
+    elif hasattr(s, "buffer"):
       return s.buffer.write(data)
     else:
       return s.write(b2s(data))                                 # TODO
   xrange = range
                                                                 # }}}1
 
-__version__       = "0.0.1"
+__version__       = "0.1.0"
 
 
 def main(*args):                                                # {{{1
@@ -126,10 +160,17 @@ def main(*args):                                                # {{{1
           file = sys.stderr)                                    # TODO
     return 2
   try:
-    if n.listen:
-      server(n.lport, n.udp)
+    if n.command:
+      p   = subprocess.Popen(n.command, shell = True,
+                             stdin  = subprocess.PIPE,
+                             stdout = subprocess.PIPE)
+      io  = p.stdout, p.stdin
     else:
-      client(n.host, n.port, n.udp)
+      io  = None
+    if n.listen:
+      server(n.lport, n.udp, io = io)
+    else:
+      client(n.host, n.port, n.udp, io = io)
   except KeyboardInterrupt:
     return 1
   return 0
@@ -150,75 +191,69 @@ def argument_parser():                                          # {{{1
   p.add_argument("--port", "-p", type = int,
                  dest = "lport", metavar = "PORT",
                  help = "local port number")
+  p.add_argument("--command", "-c",
+                 help = "shell command to exec after connect")
   p.add_argument("--test", action = "store_true",
                  help = "run tests (not netcat)")
   p.add_argument("--verbose", "-v", action = "store_true",
                  help = "run tests verbosely")
-  p.set_defaults(
-    host      = None,
-    port      = None,
-    udp       = False,
-    listen    = False,
-    lport     = None,
-    test      = False,
-    verbose   = False,
-  )
   return p
                                                                 # }}}1
 
-def client(host, port, udp = False):
+def client(host, port, udp = False, term = None, io = None):
   """connect std{in,out} to TCP (or UDP) socket (client)"""
-  set_nonblocking(sys.stdin.fileno())
+  set_nonblocking(get_io(io)[0].fileno())
   sock = create_socket(udp)
-  if not udp:
-    sock.connect((host, port))
-  handle_io(sock, udp = udp, addr = (host, port))
+  if not udp: sock.connect((host, port))
+  handle_io(sock, udp, (host, port), term, io)
 
-def server(port, udp = False):                                  # {{{1
+def server(port, udp = False, term = None, io = None):          # {{{1
   """connect std{in,out} to TCP (or UDP) socket (server)"""
 
-  set_nonblocking(sys.stdin.fileno())
-  sock = create_socket(udp)
-  sock.bind(('', port))
+  set_nonblocking(get_io(io)[0].fileno())
+  sock = create_socket(udp); sock.bind(('', port))
   if not udp:
     sock.listen(1); clientsock, addr = sock.accept()
-    handle_io(clientsock)
+    handle_io(clientsock, term = term, io = io)
   else:
-    handle_io(sock, udp = True)
+    handle_io(sock, udp = True, term = term, io = io)
                                                                 # }}}1
 
 def create_socket(udp = False):
-  return S.socket(S.AF_INET, S.SOCK_DGRAM if udp else S.SOCK_STREAM)
+  sock = S.socket(S.AF_INET, S.SOCK_DGRAM if udp else S.SOCK_STREAM)
+  sock.setsockopt(S.SOL_SOCKET, S.SO_REUSEADDR, 1)
+  return sock
 
-def handle_io(sock, udp = False, addr = None):                  # {{{1
-  """handle I/O"""
+def handle_io(sock, udp = False, addr = None, term = None,      # {{{1
+              io = None):
+  """
+  handle I/O (connect std{in/out} to socket);
+  terminates if term is passed and term.is_set()
+  """
+
+  i, o = get_io(io)
   try:
-    while True:
-      with handle_io.lock:
-        sin       = [sock, sys.stdin] if (not udp or addr) \
-                    and not sys.stdin.closed else [sock]
-      rs, _, _  = select.select(sin, [], [])
-      with handle_io.lock:
-        if sock in rs:
-          data, addr = sock.recvfrom(4096)
-          if not data: return CLOSED_SOCK
-          binwrite(sys.stdout, data)
-          sys.stdout.flush()
-        if sys.stdin in rs:
-          if sys.stdin.closed: return CLOSED_STDIN
-          data = binread(sys.stdin, 4096)
-          if udp:
-            sock.sendto(data, addr)
-          else:
-            sock.sendall(data)
+    while term is None or not term.is_set():
+      sin = [sock,i] if (not udp or addr) and not i.closed else [sock]
+      rs, _, _ = select.select(sin, [], [])
+      if sock in rs:
+        data, clientaddr = sock.recvfrom(4096)
+        if udp and not addr:
+          addr = clientaddr; sock.connect(addr)
+        if not data: return CLOSED_SOCK
+        binwrite(o, data); o.flush()
+      if i in rs:
+        data = binread(i, 4096)
+        if udp: sock.sendto(data, addr)
+        else:   sock.sendall(data)
   finally:
     sock.close()
                                                                 # }}}1
 
-handle_io.lock = threading.Lock()
+CLOSED_SOCK = "__CLOSED_SOCK__"
 
-CLOSED_SOCK, CLOSED_STDIN = \
-  "__CLOSED_SOCK__ __CLOSED_STDIN__".split()
+def get_io(io):
+  return (sys.stdin, sys.stdout) if io is None else io
 
 def set_nonblocking(fd):
   """make fd non-blocking."""
